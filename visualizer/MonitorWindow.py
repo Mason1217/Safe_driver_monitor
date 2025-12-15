936d6bf87a76f6ef1e6cbe9ad9c93f1e4c06d81e
Mason1217 <mason.chang.1212@gmail.com>
2025-12-15 19:31:36 +0800

    Display fatigue-idx pbar and past 7-day avg-hrv

--- visualizer/MonitorWindow.py
+++ visualizer/MonitorWindow.py
@@ -6,6 +6,7 @@ from PyQt6.QtWidgets import (
     QLabel,
     QFrame,
     QGridLayout,
+    QProgressBar,
 )
 
 from PyQt6.QtGui import QFont, QColor
@@ -36,7 +37,7 @@ class MonitorWindow(QMainWindow):
 
         '''
         self.setWindowTitle("Fatigue & Alcohol Monitor")
-        self.resize(1000, 700)
+        self.resize(1000, 800)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -50,14 +51,19 @@ class MonitorWindow(QMainWindow):
         info_frame = QFrame()
         info_frame.setFrameShape(QFrame.Shape.StyledPanel)
         info_layout = QGridLayout(info_frame)
+        info_layout.setVerticalSpacing(15)
+        info_layout.setHorizontalSpacing(30)
 
         value_font = QFont("Arial", 30, QFont.Weight.Bold)
         label_font = QFont("Arial", 14)
+        sub_label_font = QFont("Arial", 12)
 
         self.lbl_alcohol_title = QLabel("Alcohol")
         self.lbl_alcohol_title.setFont(label_font)
+
         self.lbl_alcohol_val = QLabel("0.00")
         self.lbl_alcohol_val.setFont(value_font)
+
         self.lbl_alcohol_status = QLabel(ALCOHOL_ST['N'])
         self.lbl_alcohol_status.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -65,18 +71,35 @@ class MonitorWindow(QMainWindow):
         self.lbl_hrv_title.setFont(label_font)
         self.lbl_hrv_val = QLabel("--")
         self.lbl_hrv_val.setFont(value_font)
+
         self.lbl_fatigue_status = QLabel(FATIGUE_ST['A'])
         self.lbl_fatigue_status.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        self.lbl_baseline = QLabel("7-Day Avg: --")
+        self.lbl_baseline.setFont(sub_label_font)
+        self.lbl_baseline.setStyleSheet("color: gray;")
+
+        self.lbl_fatigue_idx_title = QLabel("Fatigue Index (0-100)")
+        self.lbl_fatigue_idx_title.setFont(sub_label_font)
+
+        self.pbar_fatigue = QProgressBar()
+        self.pbar_fatigue.setRange(0, 100)
+        self.pbar_fatigue.setValue(0)
+        self.pbar_fatigue.setTextVisible(True)
+        self.pbar_fatigue.setFixedHeight(30)
+
         info_layout.addWidget(self.lbl_alcohol_title, 0, 0, alignment=Qt.AlignmentFlag.AlignCenter)
-        info_layout.addWidget(self.lbl_alcohol_val, 1, 0)
+        info_layout.addWidget(self.lbl_alcohol_val, 1, 0, alignment=Qt.AlignmentFlag.AlignCenter)
         info_layout.addWidget(self.lbl_alcohol_status, 2, 0)
 
         info_layout.addWidget(self.lbl_hrv_title, 0, 1, alignment=Qt.AlignmentFlag.AlignCenter)
-        info_layout.addWidget(self.lbl_hrv_val, 1, 1)
-        info_layout.addWidget(self.lbl_fatigue_status, 2, 1)
-
+        info_layout.addWidget(self.lbl_hrv_val, 1, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+        info_layout.addWidget(self.lbl_baseline, 2, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+        info_layout.addWidget(self.lbl_fatigue_status, 3, 1)
         main_layout.addWidget(info_frame)
+
+        info_layout.addWidget(self.lbl_fatigue_idx_title, 4, 0, 1, 2, alignment=Qt.AlignmentFlag.AlignBottom)
+        info_layout.addWidget(self.pbar_fatigue, 5, 0, 1, 2)
 
         self.graphWidget = pg.PlotWidget()
         self.graphWidget.setTitle("Heart Rate", color='k')
@@ -103,9 +126,12 @@ class MonitorWindow(QMainWindow):
         
         alcohol = data_dict.get(DATA_DICT_KEY["al"], 0.0)
         hrv     = data_dict.get(DATA_DICT_KEY["hrv"], 0.0)
-        rr      = data_dict.get(DATA_DICT_KEY["rr"], 0)
+        hr      = data_dict.get(DATA_DICT_KEY["hr"], -1)
         al_st   = data_dict.get(DATA_DICT_KEY["al_st"])
-        f_st    = data_dict.get(DATA_DICT_KEY["f_st"])
+        f_st    = data_dict.get(DATA_DICT_KEY["f_st"], FATIGUE_ST['A'])
+        f_idx   = data_dict.get(DATA_DICT_KEY["f_idx"], 0)
+        base    = data_dict.get(DATA_DICT_KEY["base"], 0.0)
+
 
         self.lbl_alcohol_val.setText(f"{alcohol:.2f}")
         self.lbl_alcohol_status.setText(al_st)
@@ -118,27 +144,56 @@ class MonitorWindow(QMainWindow):
             self.lbl_alcohol_status.setStyleSheet("background-color: green; color: white; padding: 5px;")
 
         self.lbl_hrv_val.setText(f"{hrv:.2f}")
+        self.lbl_baseline.setText(f"7-Day Avg: {base:.1f} ms")
         self.lbl_fatigue_status.setText(f_st)
         
-        if f_st == FATIGUE_ST['F']:
-            self.lbl_hrv_val.setStyleSheet("color: orange")
-            self.lbl_fatigue_status.setStyleSheet("background-color: orange; color: white; padding: 5px;")
+        if f_st == FATIGUE_ST['N'] or f_st == FATIGUE_ST['B']:
+            status_style = "background-color: green; color: white; padding: 5px;"
+            text_color = "color: black"
+        elif f_st == FATIGUE_ST['U']:
+            status_style = "background-color: orange; color: white; padding: 5px;"
+            text_color = "color: orange"
+        elif f_st == FATIGUE_ST['L'] or f_st == FATIGUE_ST['F']:
+            status_style = "background-color: red; color: white; padding: 5px;"
+            text_color = "color: red"
         else:
-            self.lbl_hrv_val.setStyleSheet("color: black")
-            self.lbl_fatigue_status.setStyleSheet("background-color: green; color: white; padding: 5px;")
+            status_style = "background-color: gray; color: white; padding: 5px;"
+            text_color = "color: gray"
 
-        self.update_plot(rr)
+        self.lbl_hrv_val.setStyleSheet(text_color)
+        self.lbl_fatigue_status.setStyleSheet(status_style)
 
-    def update_plot(self, rr_val):
-        '''
-        Update rr plot
-
-        '''
-        cur_bpm = 0
-        if rr_val > 0:
-            cur_bpm = 60000 / rr_val
+        self.pbar_fatigue.setValue(f_idx)
         
+        if f_idx < 40:
+            chunk_color = "#2ecc71"
+        elif f_idx < 75:
+            chunk_color = "#f39c12"
+        else:
+            chunk_color = "#e74c3c"
+
+        self.pbar_fatigue.setStyleSheet(f"""
+            QProgressBar {{
+                border: 1px solid grey;
+                border-radius: 5px;
+                text-align: center;
+            }}
+            QProgressBar::chunk {{
+                background-color: {chunk_color};
+                width: 20px;
+            }}
+        """)
+
+        self.update_plot(hr)
+
+    def update_plot(self, hr):
+        '''
+        Update heart rate plot
+
+        '''
+        if hr < MIN_HR: return
+
         self.y_data = self.y_data[1:]
-        self.y_data.append(cur_bpm)
+        self.y_data.append(hr)
 
         self.data_line.setData(self.x_data, self.y_data)
